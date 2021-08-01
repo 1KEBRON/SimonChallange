28635af63b4d36613a9e2f7ae5da7df57d33da80
kebron solomon zerie <kebron1832@hotmail.com>
2021-08-01 20:04:40 +0200

    bug fix (if inception) , clean messy code

--- game.js
+++ game.js
@@ -1,61 +1,94 @@
-var audioBlue = new Audio('sounds/blue.mp3')
-var audioGreen = new Audio('sounds/green.mp3')
-var audioRed = new Audio('sounds/red.mp3')
-var audioYellow = new Audio('sounds/yellow.mp3')
-
 var buttonColours = ["red", "blue", "green", "yellow"]
+
 var gamePattern = []
 var userClickedPattern = []
+var started = false
 var lvlNumb = 0
 
- function nextSequence(){
-    var randomNumber = Math.floor(Math.random() * 3)
-    var randomChosenColour = buttonColours[randomNumber]
-    gamePattern.push(randomChosenColour)
-    $('#'+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    playSound(randomChosenColour)
-    lvlNumb++
-     $('#level-title').html('Level ' + lvlNumb);
- }
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + lvlNumb);
+    nextSequence();
+    started = true;
+  }
+});
 
- function playSound(name){
-      switch (name) {
-        case "yellow" :
-           audioYellow.play();
-           break;
-        case "blue":
-            audioBlue.play();
-            break;
-        case "red":
-            audioRed.play();
-            break;
-        case "green":
-            audioGreen.play();
-            break;
-        default:
-            break;
-   }
-}
+
 $('.btn').click(function(){
      var userChosenColour = $(this).attr('id');
      userClickedPattern.push(userChosenColour);
+
      animatePress(userChosenColour);
      playSound(userChosenColour);
-     nextSequence();
+
+     checkAnswer(userClickedPattern.length-1);
+
+  
     
 })
+function checkAnswer(currentLevel) { 
+    
+    //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
+   if(gamePattern[currentLevel] === userClickedPattern[currentLevel] ){
+       console.log('succsess')
+  //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+      if(userClickedPattern.length == gamePattern.length){
+       //5. Call nextSequence() after a 1000 millisecond delay.
+       setTimeout(function(){
+           nextSequence();
+       },1000)
+   }
+   }
+   
+   else{
+    // console.log('wrong');
+
+    playSound('wrong');
+    
+    $('body').addClass('game-over');
+
+    setTimeout(function(){
+    $('body').removeClass('game-over');
+      },200);
+    $('#level-title').html('Game Over, Press Any Key to Restart');
+
+    startOver()
+
+   }
+   
+}
+ function nextSequence(){
+    userClickedPattern = [];
+    lvlNumb++
+    $('#level-title').html('Level ' + lvlNumb);
+
+    var randomNumber = Math.floor(Math.random() * 3)
+    var randomChosenColour = buttonColours[randomNumber]
+    gamePattern.push(randomChosenColour)
+
+    $('#'+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour)
+
+ }
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
 
 function animatePress(currentColour){
-  $('.'+currentColour).addClass('pressed')
+  $('#'+currentColour).addClass('pressed')
  setTimeout(function(){
-     $('.'+currentColour).removeClass('pressed')
+     $('#'+currentColour).removeClass('pressed')
     },100)
 }
 
-$(document).keypress(function(e){
-    var Keypressed = e.key;
-     if(Keypressed == 'a'){
-         $('#level-title').html('Level ' + lvlNumb);
-         nextSequence();
-    } 
-});
+function startOver(){
+    lvlNumb = 0;
+    gamePattern = [];
+    started = false;
+}
+
+
+
